7b5a477173f647e771a68f3efa0f9ba96e3370b3
Felipe Dantas de Araújo <72935710+felipedantas043@users.noreply.github.com>
2021-04-28 18:56:22 -0300

    falta fazer o rodape

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function validar_dados() {
+    var nome = document.getElementById('nome')
+    var email = document.getElementById('email')
+    var tel = document.getElementById('telefone')
     var res = document.getElementById('res')
 
-    res.innerHTML = "flipe"
+    res.innerHTML = "sucesso"
 }
