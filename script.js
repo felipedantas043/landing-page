a29f0ac343c004c3cf2bbb042fcaa66c0f131620
Felipe Dantas de Araújo <72935710+felipedantas043@users.noreply.github.com>
2021-04-28 22:50:47 -0300

    Enviando emails

--- script.js
+++
@@ -1,8 +0,0 @@
-function validar_dados() {
-    var nome = document.getElementById('nome')
-    var email = document.getElementById('email')
-    var tel = document.getElementById('telefone')
-    var res = document.getElementById('res')
-
-    res.innerHTML = "sucesso"
-}
